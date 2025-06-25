0897d1dd8f68d191448f3673f85f08bf8b9bb195
SimonCao1207 <hainamkaist@gmail.com>
2025-06-26 08:15:23 +0900

    implement tokenizer encoding

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,5 +1,9 @@
+import re
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+
+# Pre-tokenization pattern (used by GPT-2)
+PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
 
 class Tokenizer(ABC):
@@ -28,29 +32,43 @@ class BPETokenizer(Tokenizer):
 
     def __init__(self, params: BPETokenizerParams):
         self.params = params
+        self.special_tokens = params.special_tokens if params.special_tokens is not None else ["<|endoftext|>"]
+        self.inv_vocab: dict[bytes, int] = {v: k for k, v in params.vocab.items()}  # bytes -> index
+
+    def pre_tokenization(self, chunk: str) -> list[list[int]]:
+        """
+        Perform regex-based pre-tokenization and return a list of pre-token as a sequence of byte indices.
+        """
+
+        pattern = "|".join(re.escape(tok) for tok in self.special_tokens)
+        split_on_special_tokens = re.split(f"({pattern})", chunk)
+        ret = []
+        for span in split_on_special_tokens:
+            for match in re.finditer(PAT, span):
+                token = match.group(0)
+                token_bytes = token.encode("utf-8")
+                if token_bytes in self.special_tokens:
+                    ret.append([self.inv_vocab[token_bytes]])
+                byte_seq = [self.inv_vocab[bytes(b)] for b in token_bytes]
+                ret.append(byte_seq)
+        return ret
 
     def encode(self, string: str) -> list[int]:
-        indices = list(map(int, string.encode("utf-8")))
-        # Note: this is a very slow implementation
-        for pair, new_index in self.params.merges.items():
-            indices = merge(indices, pair, new_index)
-        return indices
+        token_ids = self.pre_tokenization(string)
+
+        for pair in self.params.merges.keys():
+            merge_index = self.params.merges[pair]
+            for ids in token_ids:
+                for i in range(len(ids) - 1):
+                    if (ids[i], ids[i + 1]) == pair:
+                        ids[i : i + 2] = [merge_index]
+                        break
+        flat_indices = [index for sublist in token_ids for index in sublist]
+        return flat_indices
 
     def decode(self, indices: list[int]) -> str:
-        bytes_list = list(map(self.params.vocab.get, indices))
-        string = b"".join(bytes_list).decode("utf-8")
-        return string
+        return ""
 
 
-def merge(indices: list[int], pair: tuple[int, int], new_index: int) -> list[int]:
-    """Return `indices`, but with all instances of `pair` replaced with `new_index`."""
-    new_indices = []
-    i = 0
-    while i < len(indices):
-        if i + 1 < len(indices) and indices[i] == pair[0] and indices[i + 1] == pair[1]:
-            new_indices.append(new_index)
-            i += 2
-        else:
-            new_indices.append(indices[i])
-            i += 1
-    return new_indices
+if __name__ == "__main__":
+    pass
